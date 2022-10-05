84792056d91dff57133c4e19ddccdbf871aa52f7
SKYUUU <blusk525@gmail.com>
2022-10-05 12:59:00 +0900

    test2

---
+++ src/example01/FieldInit.java
@@ -0,0 +1,18 @@
+package example01;
+
+public class FieldInit {
+
+    byte byteField;
+    short shortField;
+    int intField;
+    long longField;
+
+    boolean boolField;
+    char charField;
+
+    float floatField;
+    double doubleField;
+
+
+
+}
